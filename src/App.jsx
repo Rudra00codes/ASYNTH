477e41ca925a293ff00b9a11b59bdc85b0e7c6ea
Rudra <rudra17122005@gmail.com>
2025-08-25 00:57:35 +0530

    chore: remove legacy styles and scripts; update layout and responsiveness in App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,25 @@ import ProjectHighlightCard from './components/project/ProjectHighlightCard.jsx'
 const App = () => (
   <AppShell>
     {/* Left Column */}
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6 lg:gap-8 lg:sticky lg:top-6">
       <TechStackPanel />
     </div>
     {/* Middle Column */}
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6 lg:gap-8">
       <ProfileCard />
-      <div className="grid grid-cols-[120px_minmax(0,1fr)] gap-8">
-        <ToolIconRail />
+      <div className="grid gap-6 lg:grid-cols-[120px_minmax(0,1fr)]">
+        {/* On small screens, show the tool icons as a horizontal scroll rail */}
+        <div className="lg:block hidden">
+          <ToolIconRail />
+        </div>
+        <div className="lg:hidden block">
+          <ToolIconRail horizontal />
+        </div>
         <DailyToolStack />
       </div>
     </div>
     {/* Right Column */}
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6 lg:gap-8 lg:sticky lg:top-6">
       <LinksPanel />
       <ProjectHighlightCard />
     </div>
